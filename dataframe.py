3ab3b131ffaf131a9d556134945164a39b85d5e5
khj0322 <kog3480@naver.com>
2024-03-26 17:38:32 +0900

    update: dataframe.py

---
+++ dataframe.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+import streamlit as st
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("gapminder.tsv", sep="\t")
+    return df
+
+def plot_matplotlib(): # streamlit 문법
+    st.title("Categorical Bar Plot with Seaborn")
+    df = load_data()
+    fig, ax = plt.subplots() # 시각화
+    
+    # Using Seaborn's barplot function
+    sns.barplot(x=df['year'], y=df['lifeExp'], data=df, ax=ax)
+    
+    # Labeling axes and title
+    ax.set_xlabel("year")
+    ax.set_ylabel("lifeExp")
+    ax.set_title("Categorical Bar Plot")
+    
+    st.pyplot(fig) # 대시보드에 출력
+
+def main():
+    st.title("Data Display st.dataframe()")
+    st.checkbox("Use container width", value=False, key = 'use_container_width')
+    
+    df = load_data()
+    st.dataframe(df, use_container_width=True)
+
+    #pandas style
+    st.dataframe(df.iloc[:5,2:].style.highlight_max(axis=0))
+
+    plot_matplotlib() # 그래프 출력
+    
+    
+if __name__ == "__main__":
+    main()
